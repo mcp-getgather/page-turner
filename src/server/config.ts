899f3ff8a4156b83054882098d321fc54144d474
Bin Jiang <bin.jiang@heyario.com>
2025-11-11 13:59:21 -0800

    support gateway

--- src/server/config.ts
+++ src/server/config.ts
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const settings = {
   GETGATHER_URL: process.env.GETGATHER_URL || 'http://127.0.0.1:23456',
+  GETGATHER_APP_ID: process.env.GETGATHER_APP_ID || '',
   MAXMIND_ACCOUNT_ID: process.env.MAXMIND_ACCOUNT_ID || '',
   MAXMIND_LICENSE_KEY: process.env.MAXMIND_LICENSE_KEY || '',
   NODE_ENV: process.env.NODE_ENV || 'development',
